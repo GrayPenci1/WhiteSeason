4451412398d61c14e4941175243255c700eb1f1f
GrayPenci1 <oleg.kononov.01@mail.ru>
2024-12-07 16:26:22 +1000

    0.0.1/2

--- WhiteSeason/Assets/Scripts/DroneControle.cs
+++ WhiteSeason/Assets/Scripts/DroneControle.cs
@@ -24,23 +24,45 @@ public class DroneControle : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        moveDirection = CheckInputNormalized();
     }
 
-    Vector3 CheckInput()
+
+    private void FixedUpdate()
+    {
+        Mooving();
+    }
+
+    Vector3 CheckInputNormalized()
     {
         Vector3 currentDirection = Vector3.zero;
-        if (Input.GetKeyDown(KeyCode.LeftShift))
+        if (Input.GetKey(KeyCode.LeftShift))
         {
             currentDirection += Vector3.up;
         }
 
-        if (Input.GetKeyDown(KeyCode.LeftControl))
+        if (Input.GetKey(KeyCode.LeftControl))
         {
             currentDirection += Vector3.down;
         }
 
+        if (Input.GetAxisRaw("Horizontal")!= 0)
+        {
+            currentDirection.x += Input.GetAxisRaw("Horizontal");
+        }
+
+        if (Input.GetAxisRaw("Vertical") != 0)
+        {
+            currentDirection.z += Input.GetAxisRaw("Vertical");
+        }
 
         return currentDirection.normalized;
+    }
+
+    private void Mooving()
+    {
+        Vector3 addedForce = new Vector3(moveDirection.x * horizontalSpeed, moveDirection.y * verticalSpeed, moveDirection.z * horizontalSpeed);
+        RB.AddForce(addedForce,ForceMode.Impulse);
+        addedForce = Vector3.zero;
     }
 }
